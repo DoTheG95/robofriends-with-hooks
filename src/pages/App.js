2527e0e84c2277abad151cd4498c38e5f4e99981
DoTheG95 <104708516+DoTheG95@users.noreply.github.com>
2023-06-06 16:57:31 +1200

    added a counter button

--- src/pages/App.js
+++ src/pages/App.js
@@ -15,7 +15,7 @@ function App() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json()) 
         .then(users => {setRobots(users)})
-    },[])
+    },[count])
 
     const onSearchChange = (event) => {
         setSearchfield(event.target.value);
