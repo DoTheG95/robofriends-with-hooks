becb93481d65b7c278bafaa1c329b8fc6c9781e1
DoTheG95 <104708516+DoTheG95@users.noreply.github.com>
2023-06-06 14:05:53 +1200

    converted existing code to use Hooks rather than state

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,31 +1,20 @@
-import React, {Component} from "react";
+import React, {Component, useState, useEffect} from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox"
 import Scroll from '../components/Scroll';
 import './App.css'
 import ErrorBoundry from "../components/ErrorBoundry";
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App {
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => {this.setState({robots: users})})
-    }
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('');
 
     onSearchChange = (event) => {
-        this.setState({searchfield:event.target.value})
+        setSearchfield(event.target.value)
     } 
 
     render() {
-        const {robots, searchfield} = this.state;
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
